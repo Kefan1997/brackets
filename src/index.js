2a2c371c7546019078d481a6f337fb25e75702a1
Oleg Rashinskiy <oleg.rashinskiy@gmail.com>
2020-03-17 18:24:53 +0300

    fead: add solution

--- src/index.js
+++ src/index.js
@@ -8,33 +8,6 @@ module.exports = function check(str, bracketsConfig) {
             }
         }
     }
-    // let leftBracket = [];
-    // let rightBracket =[];
-
-    // for(let i = 0; i < bracketsConfig.length;i++ ){
-    //     leftBracket.push(bracketsConfig[i][0]);
-    //     rightBracket.push(bracketsConfig[i][1]);
-    //   }
-
-    // let stack = [];
-
-    // for(let i = 0; i < str.length; i++){
-
-    //     if(leftBracket.includes(str[i]) ){
-    //         stack.push(str[i]);
-    //     }
-    //     else if(rightBracket.includes(str[i]) && stack[stack.length - 1] === leftBracket[rightBracket.indexOf(str[i])]){
-    //         stack.pop();
-    //     } else{
-    //             return false;
-    //         }
-    //     if (stack.length >= 2 && (stack[stack.length - 1] == stack[stack.length - 2])){
-    //         stack.splice((stack.length - 2),2);
-    //     }
-         
-    //    console.log(stack);
-    // }
-
+    
     return str.length == 0;
-
 }
